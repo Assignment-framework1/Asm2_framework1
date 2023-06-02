0dbd97ea20e741e0209eb22dc4f26129cfdc1ec5
Thinhnvph26188 <nguyenthinhgt2@gmail.com>
2023-06-02 11:31:22 +0700

    Feat/AF-60:Create ui Admin -ui ListProduct -ui ListUser -ui AddProduct -ui EditProduct refs:AF-60

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,9 +17,13 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SigninPageComponent } from './pages/signin-page/signin-page.component';
 import { SingnupPageComponent } from './pages/singnup-page/singnup-page.component';
 import { DetailProductComponent } from './pages/detail-product/detail-product.component';
+import { ListProductComponent } from './pages/admin/list-product/list-product.component';
+import { ListUserComponent } from './pages/admin/list-user/list-user.component';
+import { AddProductComponent } from './pages/admin/add-product/add-product.component';
+import { EditProductComponent } from './pages/admin/edit-product/edit-product.component';
 
 @NgModule({
-  declarations: [AppComponent, NotFoundPageComponent, ClientLayoutComponent, AdminLayoutComponent, CartPageComponent, CheckoutPageComponent, HomePageComponent, ProductPageComponent, AboutPageComponent, SigninPageComponent, SingnupPageComponent, DetailProductComponent],
+  declarations: [AppComponent, NotFoundPageComponent, ClientLayoutComponent, AdminLayoutComponent, CartPageComponent, CheckoutPageComponent, HomePageComponent, ProductPageComponent, AboutPageComponent, SigninPageComponent, SingnupPageComponent, DetailProductComponent, ListProductComponent, ListUserComponent, AddProductComponent, EditProductComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
