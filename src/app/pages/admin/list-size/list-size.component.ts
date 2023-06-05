d87f8a87ccfdf6f892682053d196e7fe5a2793df
CuongBui157 <cuongbui1572003@gmail.com>
2023-06-06 00:21:40 +0700

    Feat/Creat list size -Vẫn đang bị lỗi chưa lấy được mảng data refs: AF-85

--- src/app/pages/admin/list-size/list-size.component.ts
+++ src/app/pages/admin/list-size/list-size.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { SizeService } from 'src/app/service/size/size.service';
+import { ISize } from 'src/interface/models';
 
 @Component({
   selector: 'app-list-size',
   templateUrl: './list-size.component.html',
-  styleUrls: ['./list-size.component.css']
+  styleUrls: ['./list-size.component.css'],
 })
 export class ListSizeComponent {
+  sizes!: ISize[];
+  constructor(private sizeService: SizeService) {
+    this.sizeService.getAll().subscribe((sizes) => console.log(sizes));
+  }
 
+  onHandleRemoveSize(id: any) {
+    // this.sizeService.deleteSize(id).subscribe(() => {
+    //   this.sizes = this.sizes.filter((size) => size._id != id);
+    // });
+  }
 }
---
+++ src/app/service/size/size.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ISize } from 'src/interface/models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SizeService {
+  private apiUrl = 'http://localhost:8080/size/size';
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<ISize[]> {
+    return this.http.get<ISize[]>(this.apiUrl);
+  }
+  getById(id: any): Observable<ISize> {
+    return this.http.get<ISize>(this.apiUrl + id);
+  }
+  addSize(data: ISize): Observable<ISize> {
+    return this.http.post<ISize>(this.apiUrl, data);
+  }
+  editSize(data: ISize): Observable<ISize> {
+    return this.http.patch<ISize>(this.apiUrl + data._id, data);
+  }
+  deleteSize(id: any): Observable<ISize> {
+    return this.http.delete<ISize>(this.apiUrl + id);
+  }
+}
