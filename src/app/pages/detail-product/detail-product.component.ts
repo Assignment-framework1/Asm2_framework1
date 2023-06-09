ae734bd139f0890bcbca0ed4ac2092713217448c
lamnta <lamntaph14414@fpt.edu.vn>
2023-06-09 21:10:52 +0700

    feat: Page ProductDetail refs: AF-89

--- src/app/pages/detail-product/detail-product.component.ts
+++ src/app/pages/detail-product/detail-product.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from 'src/app/service/product/product.service';
 
 @Component({
   selector: 'app-detail-product',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./detail-product.component.css']
 })
 export class DetailProductComponent {
-
+  products!: any;
+  quantity: number = 1;
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute) { 
+    let id = this.route.snapshot.paramMap.get('id');
+    this.route.paramMap.subscribe((params) => {
+      const id = String(params.get('id'));
+      this.productService.getById(id).subscribe((products) => {
+        this.products = products.data;
+      });
+    });
+  }
+  handleQuantity(val: string) {
+    if (this.quantity < 20 && val === 'plus') {
+      this.quantity += 1;
+    } else if (this.quantity > 1 && val === 'min') {
+      this.quantity -= 1;
+    }
+  }
 }
