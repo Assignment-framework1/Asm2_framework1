d662285d792c35e9f23eeb8ae53e936f49c7da94
Thinhnvph26188 <nguyenthinhgt2@gmail.com>
2023-06-09 20:10:22 +0700

    Feat/AF-58:Sign-up

--- src/app/pages/singnup-page/singnup-page.component.ts
+++ src/app/pages/singnup-page/singnup-page.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-singnup-page',
   templateUrl: './singnup-page.component.html',
   styleUrls: ['./singnup-page.component.css']
 })
 export class SingnupPageComponent {
+  user = {
+    username: '',
+    password: '',
+    email: '',
+    phone: '',
+    confirmpassword: ''
+  };
+  constructor(private http: HttpClient) { }
+  onSubmit() {
+    this.http.post('http://localhost:3000/signup', this.user).subscribe(
+      response => {
+        console.log('Registration successful');
+      },
+      error => {
+        console.error('Registration failed');
+      }
+    );
 
+  }
 }
---
+++ src/app/service/user/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private API_URL = 'http://localhost:8080/signup';
+
+  constructor(private http: HttpClient) { }
+  registerUser(user: { username: string, password: string }) {
+    return this.http.post(`${this.API_URL}/signup`, user);
+  }
+}
